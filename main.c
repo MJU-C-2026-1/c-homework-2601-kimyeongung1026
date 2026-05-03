6d8afdd465f35c15acb4594711ed5a243d45039c
김영웅/60231917 <kyu102684@mju.ac.kr>
2026-05-03 15:54:49 +0900

    Update main.c

--- main.c
+++ main.c
@@ -9,52 +9,64 @@
 int main()
 {
   system("chcp 65001");
-// 변수 선언
-char initial; // 고객 이니셜 1글자
-int americano_qty; // 아메리카노 수량
-int latte_qty; // 라떼 수량
-int subtotal; // 세금 포함 전 금액
-double vat; // 부가세 (소수점 계산을 위해 double 사용)
-double total; // 최종 결제 금액
+  // 변수 선언
+  int menu_choice, count, money_in;
+  int price = 0;
+  int total_price, change;
+  //1. 메뉴판 출력
+  printf("---CAFE KIOSK ---\n");
+  printf("1. 아메리카노 (2500원)\n");
+  printf("2. 카페라떼 (3000원)\n");
+  printf("메뉴 번호를 선택하세요: ");
+  scanf("%d", &menu_choice);
 
-// 1. 각 메뉴의 고정 가격
-int americano_price = 3000;
-int latte_price = 4000;
+//2. 메뉴 선택
+  if(menu_choice == 1)
+  {
+    price = 2500;
+    printf("아메리카노를 선택하셨습니다.\n");
+  }
+  else if(menu_choice == 2)
+  {
+    price = 3000;
+    printf("카페라떼를 선택하셨습니다.\n");
+  }
+  else
+  {
+    printf("없는 메뉴입니다. 주문을 종료합니다.\n");
+    return 0;
+  }
 
-// 2. 카페 메뉴판 출력
-printf("===환영합니다~ 웅이네 카페입니다!===\n");
-printf("1. 아메리카노 : %d원\n", americano_price);
-printf("2. 카페라떼 : %d원\n", latte_price);
-printf("=================================\n");
+  //3. 수량 입력 및 총액 계산
+  printf("수량을 입력하세요: ");
+  scanf("%d", &count);
 
-// 3. 주문 정보 및 이니셜 입력
-printf("고객의 이니셜(영문 1글자)을 입력해주세요: ");
-scanf("%c", &initial);
+  total_price = price * count;
+  printf("총 결제 금액은 %d원입니다.\n", total_price);
 
-printf("아메리카노의 주문 수량을 입력해주세요: ");
-scanf("%d", &americano_qty);
+  //4. 결제 및 잔돈 계산
+  printf("돈을 투입해주세요: ");
+  scanf("%d", &money_in);
 
-printf("라떼의 주문 수량을 입력해주세요: ");
-scanf("%d", &latte_qty);
+  if(money_in >= total_price)
+  {
+    change = money_in - total_price;
+    printf("결제가 완료되었습니다.\n");
+    printf("거스름돈은 %d원입니다. 감사합니다. \n", change);
 
-// 4. 금액 및 부가세 계산
-subtotal = (americano_price * americano_qty) + (latte_price * latte_qty);
-//부가세 10% 계산
-vat = subtotal * 0.1;
-//최종 금액 계산
-total = subtotal + vat;
+    //5. 영수증 출력
+    printf("--------------------\n");
+    printf("합계 금액: %d원\n", total_price);
+    printf("받은 금액: %d원\n", money_in);
+    printf("거스름돈: %d원\n", change);
+    printf("********************\n");
+    printf("이용해주셔서 감사합니다!\n");
+  }
 
-//5. 영수증 출력 
-printf("\n==========영수증==========\n");
-printf("고객 진동벨 이니셜: %c\n", initial);
-printf("_________________________\n");
-printf("아메리카노: %d잔\n", americano_qty);
-printf("라떼: %d잔\n", latte_qty);
-printf("_________________________\n");
-printf("주문 합계 : %d원\n", subtotal);
-printf("부가세(10%%): %.0f원\n", vat);
-printf("최종 결제 금액: %.0f원\n",total);
-printf("========================\n");
+  else
+  {
+    printf("잔액이 %d원 부족합니다. 주문이 취소되었습니다.\n", total_price - money_in);
+  }
 
-return 0;
+  return 0;
 }
