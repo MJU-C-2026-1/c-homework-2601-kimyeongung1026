640b8e6f5b0c832b32ffce4574dd9fc58d3a506f
김영웅/60231917 <kyu102684@mju.ac.kr>
2026-06-13 17:20:04 +0900

    Update main.c

--- main.c
+++ main.c
@@ -5,6 +5,29 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
+// 영수증을 출력하는 함수
+void printf_receipt(int cart[, int total, int moneym int change)
+{
+  printf("====== 영수증 ======\n")
+  if (cart [0] > 0) printf("아메리카노: %d잔\n", cart[0]);
+  if (cart [1] > 0) printf("카페라떼: %d잔\n", cart[1]);
+  printf("--------------------\n");
+  printf("최종 합계: %d원\n" , total);
+  printf("받은 금액: %d원\n", money);
+  printf("거스름돈 : %d원\n", change);
+  printf("********************\n");
+  printf("이용해주셔서 감사합니다!\n");
+}
+
+int calculate_distcount (int total_qty, int original_price)
+{
+  if (total_qty >= 5)
+  {
+    printf("\n 단골 감사 이벤트! 5잔 이상 구매로 10% 할인이 적용됩니다!!"\n");
+    return original_price * 0.9;
+  }
+  return original_price;
+}
 
 int main()
 {
@@ -12,18 +35,18 @@ int main()
   // 변수 선언
   int menu_choice, count, money_in;
   int total_price, change;
+  int total_qty;
 
-  //장바구니 기능을 위한 변수 
-  int americano_count = 0;
-  int latte_count = 0;
+  //장바구니 기능을 위한 배열 선언
+  int cart[2] = {0, 0};
 
   //무한 반복문
   while (1)
   {
     //1. 메뉴판 출력
     printf("---CAFE KIOSK ---\n");
-    printf("1. 아메리카노 (2500원) [현재 장바구니: %d잔]\n", americano_count);
-    printf("2. 카페라떼 (3000원) [현재 장바구니: %d잔]\n", latte_count);
+    printf("1. 아메리카노 (2500원) [현재 장바구니: %d잔]\n", cart[0]);
+    printf("2. 카페라떼 (3000원) [현재 장바구니: %d잔]\n", cart[1]);
     printf("3. 장바구니 상품 결제하기\n");
     printf("0. 프로그램 종료\n");
     printf("원하는 기능 (메뉴) 번호를 선택하세요: ");
@@ -45,7 +68,7 @@ int main()
         scanf("%d", &count);
       if (count >=1 && count <= 100)
       {
-        americano_count += count;
+        cart[0] += count;
         printf("[아메리카노 %d잔]이 장바구니에 담겼습니다.\n", count);
       }
       else
@@ -59,7 +82,7 @@ int main()
         scanf("%d", &count);
         if (count >= 1 && count <= 100)
         {
-          latte_count += count;
+          cart[1] += count;
           printf("[카페라떼 %d잔]이 장바구니에 담겼습니다.\n", count);
         }
         else
@@ -69,7 +92,7 @@ int main()
       continue;
 
       case 3:
-      if(americano_count == 0 && latte_count == 0)
+      if(cart[0] == 0 && cart[1] == 0)
       {
         printf("장바구니가 비어있습니다. 메뉴를 먼저 선택해주세요.\n");
         continue;
@@ -81,16 +104,17 @@ int main()
         continue;
   }
   //3. 총 결제 및 금액 계산
-  total_price = (americano_count * 2500) + (latte_count * 3000);
-
+  total_price = (cart[0] * 2500) + (cart[1] * 3000);
+  total_qty = cart[0] + cart[1];
+    
     printf("--- [주문 내역 확인] ---\n");
-    if (americano_count > 0)
+    if (cart[0] > 0)
     {  
-      printf("아메리카노: %d잔\n", americano_count);
+      printf("아메리카노: %d잔\n", cart[0]);
     }
-    if ( latte_count > 0)
+    if (cart[1] > 0)
     {
-      printf("카페라떼: %d잔\n", latte_count);
+      printf("카페라떼: %d잔\n", cart[1]);
     }
     printf("총 결제 금액은 %d원입니다.\n", total_price);
     printf("-----------------------\n");
@@ -105,17 +129,12 @@ int main()
       printf("결제가 완료되었습니다.\n");
       printf("거스름돈은 %d원입니다. 감사합니다. \n", change);
 
-      //5. 영수증 출력
-      printf("--------------------\n");
-      printf("합계 금액: %d원\n", total_price);
-      printf("받은 금액: %d원\n", money_in);
-      printf("거스름돈: %d원\n", change);
-      printf("********************\n");
-      printf("이용해주셔서 감사합니다!\n");
+      //5. 영수증 출력 함수 호출
+      printf_receipt(cart, total_price, money_in, change);
       
       // 장바구니 초기화
-      americano_count = 0;
-      latte_count = 0;
+      cart[0] = 0;
+      cart[1] = 0;
     }
 
     else
